0d702bc3957da3f0dda06a194681691b94e9733c
GaneshSansoa <gchaudhary1995@gmail.com>
2019-10-06 21:19:30 +0530

    Working Module

--- get_charts.php
+++ get_charts.php
@@ -5,14 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/fontawesome.js"
-     integrity="sha384-7ox8Q2yzO/uWircfojVuCQOZl+ZZBg2D2J5nkpLqzH1HY0C1dHlTKIbpRz/LG23c" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <title>Starter Template for Bootstrap</title>
 
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
 <link href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.css" rel="stylesheet">
+<link rel="stylesheet" href="css/dateTimePicker.css">
     <!-- Custom styles for this template -->
     <link href="css/style.css" rel="stylesheet">
   </head>
@@ -59,29 +59,62 @@
         </div>
     </nav>
 
-    <main role="main" class="container">
+    <main role="main" class="container-fluid" id="page-container">
 
       <div class="container">
 		<div class="row justify-content-center">
-		  <div class="col-12">
+        <h4 id="show_data"></h4>
+            <div class="col-6">
             <form action="" id="upload_req" onsubmit="return false;">
-            <input type="text" name="starts">
-                <input type="text" name="ends">
-            <input type="submit" class="btn btn-dark" value="Submit">
+            <div class="row justify-content-center">
+            <div class="col-6 form-group">
+            <label for="starts">Start Date:</label>
+            <input type="text" name="starts" class="form-control" id="example1">
+            </div>
+            <div class="col-6 form-group">
+            <label for="ends">End Date:</label>
+            <input type="text" name="ends" class="form-control" id="example2">
+            </div>
+            
+            <div class="col-6 form-group">
+            <input type="submit" class="btn btn-dark col-12 form-control" value="Submit">
+            </div>
+            
+
+
+            </div>
+            
+                <!-- <input type="text" name="ends">
+                <input type="text" class="form-control" id="example"> -->
             </form>
 
-		  </div>
-          <div class="col-6">
-          <canvas id="myChart" width="" height=""></canvas>
-          </div>
+		      </div>
+
 	  
-		</div>
-	
+	    	</div>
+        
    
-      </div>
+          </div>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-12" id="res">
+                    <h4>Results:</h4>
+                    <canvas id="myChart" width="" height=""></canvas>
+                </div>
+            </div>
+
+        </div>
+
+        </div>
+        </div>
+
 
     </main><!-- /.container -->
-
+    <div class="container-fluid" id="footer">
+        <footer class="container">
+            <center>Copyright @ <?php echo Date("Y", strtotime("now"));?></center>
+        </footer>    
+        </div>    
 
     <!-- Bootstrap core JavaScript
     ================================================== -->
@@ -91,11 +124,37 @@
     integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="js/bootstrap.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js"></script>
-	<script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment-with-locales.js"></script>
+    <script src="js/datetimePicker.js"></script>
+    <script>
 	$(document).ready(function(){
+    $("#res").hide();
+    var min_date,max_date;
+        $.ajax({
+            url:'get_res.php',
+            type:'GET',
+            dataType:'json',
+            success:function(res){
 
-
+                $("#show_data").html("Data is Available from: " + res.minDate + " to " + res.maxDate);
+                min_date = res.minDate;
+                max_date = res.maxDate;
+                console.log(min_date);
+                $('#example1').datetimepicker({
+                    format:'YYYY/MM/DD HH:mm:ss',
+                    sideBySide:true,
+                    minDate:min_date,
+                    maxDate:max_date            
+                });
+                $('#example2').datetimepicker({
+                    format:'YYYY/MM/DD HH:mm:ss',
+                    sideBySide:true,
+                    minDate:min_date,
+                    maxDate:max_date,
+                });
+            }
+        });
 
  		$("#upload_req").submit(function(){
             $.ajax({
@@ -103,13 +162,20 @@
                 type:"POST",
                 data:$(this).serialize(),
                 success:function(res){
+                    $("#res").show();
                     var time = [];
                     var inside_vpd = [];
                     var outside_vpd = [];
                     for(var i = 0; i < res.length; i++){
-                        time[i] = res[i].Date;
-                        var sat_vap_pressure = 0.7392 * Math.exp(0.06264 * res[i].Tapc * (Math.exp(-0.0019 * time[i])));
-                        var actual_vap_pressure = 0.8427 * (res[i].Eapc) / 100 * Math.exp(0.06264 * res[i].Tapc * Math.exp(-0.0019 * time[i]) - 0.00021 * time[i]);
+                        if(res[i].Date == "0"){
+                            time[i] = res[i].Date1;
+                            // console.log("Fired...");
+                        }
+                        else{
+                            time[i] = res[i].Date + ":00";
+                        }
+                        var sat_vap_pressure = 0.7392 * Math.exp(0.06264 * res[i].Tapc * (Math.exp(-0.0019 * res[i].Date)));
+                        var actual_vap_pressure = 0.8427 * (res[i].Eapc) / 100 * Math.exp(0.06264 * res[i].Tapc * Math.exp(-0.0019 * res[i].Date) - 0.00021 * res[i].Date);
                          inside_vpd[i] = sat_vap_pressure - actual_vap_pressure;
                         // inside_temp[i] = res[i].Tapc;
                         // inside_hum = res[i].Eapc;
@@ -147,7 +213,8 @@
 
                         }]
                     },
-                    responsive:true
+                    responsive:true,
+                    intersect:true,
                 }
             });
                 }
